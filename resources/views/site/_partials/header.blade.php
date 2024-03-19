5437c1fd4697907bdb1d8490a492f31da3cbfdd2
joaomariussi <joaomariussi10@gmail.com>
2024-03-19 15:03:58 -0300

    :lipstick: Separando as views para se tornar mais prático.

---
+++ resources/views/site/_partials/header.blade.php
@@ -0,0 +1,9 @@
+<div class="menu">
+    <nav>
+        <a href="{{ route('site.home') }}">Início</a>
+        <a href="#">Serviços</a>
+        <a href="#">Contato</a>
+        <a href="{{ route('logout') }}">Sair</a>
+    </nav>
+</div>
+
