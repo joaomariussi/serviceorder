f5339146509f91868f3d82e9226dd3d0ed898492
joaomariussi <joaomariussi10@gmail.com>
2024-03-29 16:40:23 -0300

    :safety_vest: Adicionando a variável $user para todas as views.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Compartilhando a variável $user com todas as views
+        View::composer('*', function ($view) {
+            $user = Auth::user();
+            $view->with('user', $user);
+        });
     }
 }
