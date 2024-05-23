6907c3cffb72a0a82cd5e63babf0f057c263cab5
joaomariussi <joaomariussi10@gmail.com>
2024-05-23 08:47:21 -0300

    :lipstick: Ajustando a estilização de algumas views.

--- resources/views/site/servico-finalizado.blade.php
+++ resources/views/site/servico-finalizado.blade.php
@@ -7,20 +7,18 @@
 @section('conteudo')
     @include('site._partials.header')
 
-    <div class="main-content">
-        <div class="container-sucesso">
-            <div class="container">
+    <main class="main-content">
+        <section class="container-sucesso">
+            <div class="content">
                 <h1>Serviço Finalizado com Sucesso</h1>
                 <p>O seu serviço foi finalizado com sucesso. Obrigado por utilizar nossos serviços!</p>
                 <div class="botoes-navegacao">
                     <button type="button" onclick="window.location.href='{{ route('site.home') }}'" class="button-voltar">
                         Voltar para Home
                     </button>
-                    <div class="button-container">
-                        <a href="{{ route('site.servico-pdf') }}" class="btn btn-imprimir">Visualizar Serviço</a>
-                    </div>
+                    <a href="{{ route('site.servico-pdf') }}" class="btn btn-imprimir">Visualizar Serviço</a>
                 </div>
             </div>
-        </div>
-    </div>
+        </section>
+    </main>
 @endsection
